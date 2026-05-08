44c8479f66010c113631965fc444556c86c7c4e9
DarioZancuo <dario.zancuo@hotmail.it>
2026-05-08 17:14:11 +0200

    added units tests

--- src/test/java/testing/TestingUnit.java
+++ src/test/java/testing/TestingUnit.java
@@ -4,20 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.kataelevator.model.Direction;
+import it.kataelevator.model.DoorState;
 import it.kataelevator.model.Elevator;
 import it.kataelevator.service.ElevatorService;
 
 class TestingUnit {
+	
+    private ElevatorService service;
+    private Elevator elevator;
+	
+	@BeforeEach
+    void setup() {
+		//setup prima di ogni test
+        service = new ElevatorService();
+        elevator = new Elevator(1);
+        service.registerElevator(elevator);
+    }
+	
 	@Test
 	void testElevatorReachesTargetFloor() {
 		
-		//imposto 1 ascensore
-		ElevatorService service = new ElevatorService();
-		Elevator lift = new Elevator(1);
-		service.registerElevator(lift);
-
 		//scelgo un piano
 		int targetFloor = 2;
 		service.requestFloor(targetFloor);
@@ -25,25 +35,23 @@ class TestingUnit {
 		//simulo gli step
 		int maxSteps = 10;
 		int steps = 0;
-		while (lift.hasStops() && steps < maxSteps) {
+		while (elevator.hasStops() && steps < maxSteps) {
 			service.simulateStep();
 			steps++;
 		}
 
 		//testo il piano raggiunto
-		assertEquals(targetFloor, lift.getCurrentFloor(),
+		assertEquals(targetFloor, elevator.getCurrentFloor(),
 				"L'ascensore dovrebbe aver raggiunto il piano " + targetFloor);
 
 		//testo lo stato della porta
-		assertEquals(it.kataelevator.model.DoorState.OPEN, lift.getDoorState(),
+		assertEquals(DoorState.OPEN, elevator.getDoorState(),
 				"Le porte dovrebbero aprirsi una volta raggiunto il piano");
 	}
 
 	@Test
 	void checkIfFloorExists() {
 		
-		Elevator elevator = new Elevator(1);
-
 		//  piani validi
 		assertTrue(elevator.addStop(0), "Il piano 0 dovrebbe essere valido");
 		assertTrue(elevator.addStop(10), "Il piano 10 dovrebbe essere valido");
@@ -52,5 +60,45 @@ class TestingUnit {
 		// piani non esistenti
 		assertFalse(elevator.addStop(-1), "Un piano negativo non dovrebbe essere accettato");
 		assertFalse(elevator.addStop(11), "Un piano superiore a 10 non dovrebbe essere accettato");
+	}
+	
+	@Test
+	void invalidFloorTest() {
+		
+	    boolean added = elevator.addStop(11);
+	    elevator.step();
+
+	    //se il piano inserito non è valido addStop torna false
+	    assertFalse(added);
+	    
+	    //ascensore non si muove 
+	    assertEquals(0, elevator.getCurrentFloor());
+	    assertEquals(Direction.IDLE, elevator.getDirection());
+	}
+	
+	@Test
+	void elevatorMovesUpTest() {
+		
+	    elevator.addStop(3);
+	    elevator.step();
+
+	    //faccio solo 1 step quindi ascensore si muove in alto di 1
+	    assertEquals(1, elevator.getCurrentFloor());
+	    assertEquals(Direction.UP, elevator.getDirection());
+	}
+	
+	@Test
+	void CloseDoorsTest() {
+		
+	    elevator.addStop(1);
+	    elevator.step(); 
+	    
+	    //quando arriva OPEN
+	    assertEquals(DoorState.OPEN, elevator.getDoorState());
+
+	    elevator.step(); 
+	    
+	    //il metodo step cambiare DoorState a CLOSED
+	    assertEquals(DoorState.CLOSED, elevator.getDoorState());
 	}
 }
